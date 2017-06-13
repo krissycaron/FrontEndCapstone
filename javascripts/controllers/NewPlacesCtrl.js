5ed7512009abf11860d53196ad04ee70db24d9a7
Kristin Caron <krissycaron@gmail.com>
2017-06-12 19:37:36 -0500

    stuck on logging in a user after register

---
+++ javascripts/controllers/NewPlacesCtrl.js
@@ -0,0 +1,5 @@
+app.controller("NewPlacesCtrl", ($scope) => {
+console.log("inside the NewPlacesCtrl");
+///this is the search view will mimic the visual return of the "cards" with the static map, name, phone# ... 
+
+});
