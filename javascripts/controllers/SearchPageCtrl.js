6e92e6ecc9fd8138b3df3c0a195a4298375003bd
Kristin Caron <krissycaron@gmail.com>
2017-06-13 20:59:18 -0500

    view specific item

--- javascripts/controllers/SearchPageCtrl.js
+++ javascripts/controllers/SearchPageCtrl.js
@@ -2,6 +2,9 @@ app.controller("SearchPageCtrl", function($scope, GoogleFactory,){
 console.log("inside seach controller");
 	$scope.searchPlaces = "";
 	$scope.places = [];
+	$scope.newPlace = {};
+
+	$scope.newForm = false;
 
 	$scope.returnPlaces= ()=>{
 		GoogleFactory.getPlaces($scope.searchPlaces)
@@ -13,6 +16,19 @@ console.log("inside seach controller");
 			console.log("Error in returnPlaces", error);
 		});
 	};
+
+	$scope.newView = (place) =>{
+		//left hand side is the seed, right is value from api
+		$scope.newPlace.name =place.name;
+		$scope.newPlace.vicinity =place.vicinity;
+		$scope.newPlace.googleId =place.place_id;
+		console.log($scope.newPlace);
+		$scope.newForm=true;
+		}
+
+	$scope.setCategory = (clickput) => {
+		$scope.newPlace.category = clickput;
+	}
 
 
 });
--- javascripts/factories/PlacesFactory.js
+++ javascripts/factories/PlacesFactory.js
@@ -1,3 +1,21 @@
+app.factory("PlacesFactory", function($q, $http, GOOGLE_PLACES, FIREBASE_CONFIG){
 
 
-// https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.174465, -86.767960&rankby=distance&keyword=fido&key=yourkey
+   let postNewItem = () =>{
+    return $q((resolve, reject) => {
+      $http.post(`${FIREBASE_CONFIG.databaseURL}/places.json`, JSON.stringify())
+      .then((resultz)=>{
+        resolve(resultz);
+      }).catch((error)=>{
+        reject(error);
+      });
+    });
+  };
+
+
+
+
+
+});
+
+
