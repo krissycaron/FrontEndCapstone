a233af8dd791218aece62dc9897295da92c1ae30
Kristin Caron <krissycaron@gmail.com>
2017-06-14 20:57:13 -0500

    getuserslist view is written need to implement it

--- javascripts/controllers/ViewUserPlacesCtrl.js
+++ javascripts/controllers/ViewUserPlacesCtrl.js
@@ -1,4 +1,32 @@
-app.controller("ViewUserPlacesCtrl", function($scope, $location, $q, $http, PlacesFactory, GoogleFactory, ) {
+app.controller("ViewUserPlacesCtrl", function($scope, $location, $q, $http, PlacesFactory, GoogleFactory) {
 console.log("inside the ViewUserPlacesCtrl");
 ///this is the users saved places view
+	$scope.places = [];
+
+	let getUsersPlaces = () => {
+		PlacesFactory.getPlacesList($rootScope.user.uid)
+		.then((placez) => {
+			console.log("placez", placez);
+			$scope.places = placez; 
+		}).catch((error)=>{
+			console.log("error in getUsersPlaces", error);
+		});
+	}
+
+
 });
+
+
+///==============================
+
+
+// $scope.items = [];
+      
+//     let getItems = () =>{
+//       itemFactory.getItemList($rootScope.user.uid).then((itemz)=>{
+//         console.log("items", itemz);
+//         $scope.items = itemz;
+//         }).catch((error)=>{
+//         console.log("got and error", error);
+//         });
+//       };
