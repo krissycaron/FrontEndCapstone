04533f561f25c717d6a268b12039abe0d930600f
Kristin Caron <krissycaron@gmail.com>
2017-07-11 09:03:38 -0500

    styling and heroku completed

--- javascripts/factories/GoogleFactory.js
+++ javascripts/factories/GoogleFactory.js
@@ -3,7 +3,7 @@ app.factory("GoogleFactory", function($q, $http, GOOGLE_PLACES){
 	let getPlaces = (userInput)=>{
       return $q((resolve, reject) => {
         // $.ajax().done().fail ... this is what we were using. nad becasue there is another lib you need to put in the argument. 
-        $http.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.174465, -86.767960&rankby=distance&keyword=${userInput}&key=${GOOGLE_PLACES}`)
+        $http.get(`https://pet-places.herokuapp.com/api/petPlaces?location=36.174465, -86.767960&rankby=distance&keyword=${userInput}&key=${GOOGLE_PLACES}`)
       
         .then((googleData)=> {
             resolve(googleData);
